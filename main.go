baf4dd64b24ea5933aa36765a47119a2992eba98
Marlon Fagundez <mjfagundez@gmail.com>
2023-03-06 13:54:33 -0500

    Add insertData function

--- main.go
+++ main.go
@@ -24,6 +24,14 @@ func createTable(ctx context.Context, conn *pgx.Conn) error {
 	return err
 }
 
+func insertData(ctx context.Context, conn *pgx.Conn, name string, weight int64) error {
+	_, err := conn.Exec(ctx, "INSERT INTO widgets (name, weight) VALUES ($1, $2)", "Freeman", 80)
+	if err != nil {
+		handleError("unable to insert data", err, false)
+	}
+	return err
+}
+
 func run(ctx context.Context, dbURL string) error {
 	conn, err := pgx.Connect(ctx, dbURL)
 	if err != nil {
@@ -36,9 +44,14 @@ func run(ctx context.Context, dbURL string) error {
 		handleError("unable to create table", err, false)
 	}
 
+	err = insertData(ctx, conn, "widget1", 100)
+	if err != nil {
+		handleError("unable to insert data", err, false)
+	}
+
 	var name string
 	var weight int64
-	err = conn.QueryRow(ctx, "select name, weight from widgets where id=$1", 42).Scan(&name, &weight)
+	err = conn.QueryRow(ctx, "select name, weight from widgets where =$1", 42).Scan(&name, &weight)
 	if err != nil {
 		handleError("query row failed", err, false)
 	}
